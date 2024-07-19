3c276c4adf6aca25a2d0dc82c4d8c49e04a2e5c5
codecommitUser+1-at-726466400073 <277402816@qq.com>
2024-07-19 14:40:13 +0800

    Update 5-nginx-ingress.tf

--- infrastructure/terraform/5-nginx-ingress.tf
+++ infrastructure/terraform/5-nginx-ingress.tf
@@ -1,9 +1,6 @@
 data "azurerm_kubernetes_cluster" "this" {
-  name                = var.aks_name
-  resource_group_name = local.resource_group_name
-
-  # Comment this out if you get: Error: Kubernetes cluster unreachable 
-  depends_on = [azurerm_kubernetes_cluster.aks]
+  name                = azurerm_kubernetes_cluster.aks.name
+  resource_group_name = azurerm_kubernetes_cluster.aks.resource_group_name
 }
 
 provider "helm" {
