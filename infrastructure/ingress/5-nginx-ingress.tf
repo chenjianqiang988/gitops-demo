6cc93c4d41ace4fddf516185e541a33c421ab4a1
codecommitUser+1-at-726466400073 <277402816@qq.com>
2024-07-19 16:31:11 +0800

    update

--- infrastructure/ingress/5-nginx-ingress.tf
+++ infrastructure/ingress/5-nginx-ingress.tf
@@ -6,10 +6,7 @@ data "azurerm_kubernetes_cluster" "this" {
 
 provider "helm" {
   kubernetes {
-    host                   = data.azurerm_kubernetes_cluster.this.kube_config.0.host
-    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.this.kube_config.0.client_certificate)
-    client_key             = base64decode(data.azurerm_kubernetes_cluster.this.kube_config.0.client_key)
-    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.this.kube_config.0.cluster_ca_certificate)
+    config_path = "~/.kube/config"
   }
 }
 
