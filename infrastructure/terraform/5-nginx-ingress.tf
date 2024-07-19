58241ad0cca10055655aed1ea4f93403e324d339
codecommitUser+1-at-726466400073 <277402816@qq.com>
2024-07-19 14:12:27 +0800

    update code

---
+++ infrastructure/terraform/5-nginx-ingress.tf
@@ -0,0 +1,31 @@
+data "azurerm_kubernetes_cluster" "this" {
+  name                = var.aks_name
+  resource_group_name = local.resource_group_name
+
+  # Comment this out if you get: Error: Kubernetes cluster unreachable 
+  depends_on = [azurerm_kubernetes_cluster.this]
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.azurerm_kubernetes_cluster.this.kube_config.0.host
+    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.this.kube_config.0.client_certificate)
+    client_key             = base64decode(data.azurerm_kubernetes_cluster.this.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.this.kube_config.0.cluster_ca_certificate)
+  }
+}
+
+resource "helm_release" "external_nginx" {
+  name = "external"
+
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  namespace        = "ingress"
+  create_namespace = true
+  version          = "4.11.1"
+
+  set {
+    name  = "controller.service.type"
+    value = "LoadBalancer"
+  }
+}
