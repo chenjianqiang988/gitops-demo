be37d88c2978cc692684a7c1af8ac78c6c393a69
codecommitUser+1-at-726466400073 <277402816@qq.com>
2024-07-22 09:44:28 +0800

    update code

---
+++ infrastructure/terraform/5-nginx-ingress.tf
@@ -0,0 +1,26 @@
+provider "helm" {
+  kubernetes {
+    host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
+  }
+}
+
+resource "helm_release" "external_nginx" {
+  depends_on = [
+    azurerm_kubernetes_cluster.aks
+  ]
+  name = "ingress-nginx"
+
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  namespace        = "ingress"
+  create_namespace = true
+  version          = "4.11.1"
+
+  set {
+    name  = "controller.service.type"
+    value = "LoadBalancer"
+  }
+}
